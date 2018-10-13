ee8a7a59cdd6b51ed607c15ae46f2cfdf7a4e78c
Stuart Campbell <stuartcampbell@Stuarts-iMac-2.local>
2018-10-13 14:40:05 -0500

    Begin Again

---
+++ src/components/pages/Mainpage.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class mainPage extends React.Component{
+    render(){
+        return (
+        <h1>Hello</h1>
+        );       
+    }
+}
+
+export default mainPage;
